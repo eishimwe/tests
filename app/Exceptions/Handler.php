988a59e6d6a85a43b0f104e0b6a00531f07987e8
Elie Ishimwe <elie.ishimwe@ignitiongroup.co.za>
2016-06-21 05:48:24 +0200

    updates

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,8 +46,13 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $e)
     {
 
-        if ( $e instanceof \Illuminate\Session\TokenMismatchException ) {
+     /*   if ( $e instanceof \Illuminate\Session\TokenMismatchException ) {
             return redirect()->route('login');
+        }
+*/
+        if ($e instanceof \Illuminate\Session\TokenMismatchException) {
+
+            return redirect()->back()->withInput()->with('token', csrf_token());
         }
 
         return parent::render($request, $e);
