70ecab97a861ecc329e0eadbb5da1db339157659
Elie Ishimwe <elie.ishimwe@ignitiongroup.co.za>
2016-07-02 17:57:33 +0200

    added cronjob

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,12 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\User;
+use App\TransactionPayout;
+use App\Transaction;
+use App\DonationAllocation;
+use App\UserTransaction;
+
 
 class Kernel extends ConsoleKernel
 {
@@ -26,5 +32,136 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+
+
+        $schedule->call(function () {
+
+
+            $payouts = \DB::table('transactions_payouts')
+                                ->select(
+                                \DB::raw(
+                                    "
+                                     `transactions_payouts`.created_at,
+                                     `transactions_payouts`.id,
+                                     `transactions_payouts`.payout_amount,
+                                     `transactions_payouts`.transaction_id                                   
+                                   
+                                    "
+
+                            )
+                            )->orderBy('created_at','asc')
+                            ->get();
+
+
+            $donations = \DB::table('donations')
+                                ->select(
+                                \DB::raw(
+                                    "
+                                     `donations`.created_at,
+                                     `donations`.id,
+                                     `donations`.donation_amount,
+                                     `donations`.user_id                                   
+                                   
+                                    "
+
+                            )
+                            )->orderBy('created_at','asc')
+                            ->get();
+
+
+            foreach ($payouts as $payout) {
+
+
+                $transaction_payout_amount = $payout->payout_amount;      
+                $user_transaction          = UserTransaction::where('transaction_id',$payout->transaction_id)->first();
+
+
+                if (sizeof($donations) > 0) {
+
+
+                    foreach ($donations as $donation) {
+
+
+                        if($donation->donation_amount <= $transaction_payout_amount ) {
+
+
+                            $donation_allocation                    = new DonationAllocation();
+                            $donation_allocation->donor_id          = $donation->user_id;
+                            $donation_allocation->receiver_id       = $user_transaction->user_id;
+                            $donation_allocation->donation_amount   = $donation->donation_amount;
+                            $donation_allocation->save();
+                            $transaction_payout_amount              = $transaction_payout_amount - $donation->donation_amount;
+
+
+                        } else {
+
+                             $donation_allocation                    = new DonationAllocation();
+                             $donation_allocation->donor_id          = $donation->user_id;
+                             $donation_allocation->receiver_id       = $user_transaction->user_id;
+                             $donation_allocation->donation_amount   = $donation->donation_amount;
+                             $donation_allocation->save();
+
+                             $transaction_payout_amount              = $transaction_payout_amount - $donation->donation_amount;
+
+
+
+
+                        }
+
+                        if ($transaction_payout_amount <= 0) {
+
+
+                             $transaction = Transaction::find($payout->transaction_id);
+                             $transaction->transaction_type_id = 3; 
+                             $transaction->save();
+                             break;
+                             
+
+
+
+
+                        } else {
+
+
+                            $transaction = Transaction::find($payout->transaction_id);
+                            $transaction->transaction_type_id = 2; 
+                            $transaction->save();
+
+
+
+
+                        }
+
+
+
+                        
+                    
+                    }
+
+
+                }
+                else {
+
+
+                    $transaction = Transaction::find($payout->transaction_id);
+                    $transaction->transaction_type_id = 2; 
+                    $transaction->save();
+                    
+
+                }
+
+                
+
+            }
+            
+
+            $user = User::find(15);
+            $user->first_name = "Kapanga Intelligence";
+            $user->save();
+
+
+
+
+        });
     }
 }
