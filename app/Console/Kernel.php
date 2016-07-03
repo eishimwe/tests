c9c5c9c424f7573ad165c24336edc2463d7e0b0c
Elie Ishimwe <elie.ishimwe@ignitiongroup.co.za>
2016-07-03 18:43:05 +0200

    updates on cronjob

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -33,155 +33,152 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
        
-
-
-        $schedule->call(function () {
+             $schedule->call(function () {
 
 
 
-            $payouts = \DB::table('transactions_payouts')
-                                ->select(
-                                \DB::raw(
-                                    "
-                                     `transactions_payouts`.created_at,
-                                     `transactions_payouts`.id,
-                                     `transactions_payouts`.payout_amount,
-                                     `transactions_payouts`.transaction_id                                   
-                                   
-                                    "
+                $payouts = \DB::table('transactions_payouts')
+                                    ->select(
+                                    \DB::raw(
+                                        "
+                                         `transactions_payouts`.created_at,
+                                         `transactions_payouts`.id,
+                                         `transactions_payouts`.payout_amount,
+                                         `transactions_payouts`.transaction_id                                   
+                                       
+                                        "
 
-                            )
-                            )->orderBy('created_at','asc')
-                            ->get();
-
-
-            $donations = \DB::table('donations')
-                                ->where('is_valid','=',1)
-                                ->select(
-                                \DB::raw(
-                                    "
-                                     `donations`.created_at,
-                                     `donations`.id,
-                                     `donations`.donation_amount,
-                                     `donations`.user_id                                   
-                                   
-                                    "
-
-                            )
-                            )
-                            ->orderBy('created_at','asc')
-                            ->get();
-
-            //dd($donations);
+                                            )
+                                    )->orderBy('created_at','asc')
+                                    ->get();
 
 
-            foreach ($payouts as $payout) {
+                    $donations = \DB::table('donations')
+                                       ->where('is_valid','=',1)
+                                        ->select(
+                                                \DB::raw(
+                                                    "
+                                                     `donations`.created_at,
+                                                     `donations`.id,
+                                                     `donations`.donation_amount,
+                                                     `donations`.user_id,
+                                                     `donations`.is_valid                                    
+                                                   
+                                                    "
+
+                                                    )
+                                                )
+                                        ->orderBy('created_at','asc')
+                                        ->get();
+
+          
 
 
-                $transaction_payout_amount = $payout->payout_amount;      
-                $user_transaction          = UserTransaction::where('transaction_id',$payout->transaction_id)->first();//R500
+                        foreach ($payouts as $payout) {
 
 
-                if (sizeof($donations) > 0) {
+                            $transaction_payout_amount = $payout->payout_amount;      
+                            $user_transaction          = UserTransaction::where('transaction_id',$payout->transaction_id)->first();
 
 
-                    foreach ($donations as $donation) {
+                                if (sizeof($donations) > 0) {
 
 
-                        if($donation->donation_amount <= $transaction_payout_amount ) {
+                                    foreach ($donations as $donation) {
 
 
-                            $donation_allocation                    = new DonationAllocation();
-                            $donation_allocation->donor_id          = $donation->user_id;
-                            $donation_allocation->receiver_id       = $user_transaction->user_id;
-                            $donation_allocation->donation_amount   = $donation->donation_amount;
-                            $donation_allocation->save();
-                            $transaction_payout_amount              = $transaction_payout_amount - $donation->donation_amount;
+                                        if($donation->donation_amount <= $transaction_payout_amount ) {
+
+
+                                            $donation_allocation                    = new DonationAllocation();
+                                            $donation_allocation->donor_id          = $donation->user_id;
+                                            $donation_allocation->receiver_id       = $user_transaction->user_id;
+                                            $donation_allocation->donation_amount   = $donation->donation_amount;
+                                            $donation_allocation->save();
+                                            $transaction_payout_amount              = $transaction_payout_amount - $donation->donation_amount;
+
+                                            
+                                            $objPayout                              = TransactionPayout::find($payout->id);
+                                            $objPayout->payout_amount               = $transaction_payout_amount; 
+                                            $objPayout->save();
+
+
+                                            $objDonation                              = Donation::find($donation->id);
+                                            $objDonation->is_valid                    = 0; 
+                                            $objDonation->save();
+
+
+                                        } else {
+
+                                             
+                                            
+
+                                             $donation_allocation                    = new DonationAllocation();
+                                             $donation_allocation->donor_id          = $donation->user_id;
+                                             $donation_allocation->receiver_id       = $user_transaction->user_id;
+                                             $donation_allocation->donation_amount   = $transaction_payout_amount;
+                                             $donation_allocation->save();
+
+                                             $transaction_payout_amount              = $transaction_payout_amount - $transaction_payout_amount;
+                                             $objPayout                              = TransactionPayout::find($payout->id);
+                                             $objPayout->payout_amount               = $transaction_payout_amount;
+                                             $objPayout->save();
+
+
+                                             //$objDonation                              = Donation::find($donation->id);
+                                             //$objDonation->is_valid                    = 0; 
+                                             //$objDonation->save();
+
+
+
+
+                                        }
+
+                                        if ($transaction_payout_amount <= 0) {
+
+
+                                             $transaction = Transaction::find($payout->transaction_id);
+                                             $transaction->transaction_type_id = 3; 
+                                             $transaction->save();
+                                             break;
+                                             
+
+
+
+
+                                        } else {
+
+
+                                            $transaction = Transaction::find($payout->transaction_id);
+                                            $transaction->transaction_type_id = 2; 
+                                            $transaction->save();
+
+
+
+
+                                        }
+
+
+
+                                        
+                                    
+                                }
+
+
+                                }
+                                else {
+
+
+                                    $transaction = Transaction::find($payout->transaction_id);
+                                    $transaction->transaction_type_id = 2; 
+                                    $transaction->save();
+                                    
+
+                                }
 
                             
-                            $objPayout                              = TransactionPayout::find($payout->id);
-                            $objPayout->payout_amount               = $transaction_payout_amount; 
-                            $objPayout->save();
-
-
-                            $objDonation                              = Donation::find($donation->id);
-                            $objDonation->is_valid                    = 0; 
-                            $objDonation->save();
-
-
-                        } else {
-
-                             
-                            
-                             $amount_to_donate                       =  $donation->donation_amount - $transaction_payout_amount;
-
-                             $donation_allocation                    = new DonationAllocation();
-                             $donation_allocation->donor_id          = $donation->user_id;
-                             $donation_allocation->receiver_id       = $user_transaction->user_id;
-                             $donation_allocation->donation_amount   = $amount_to_donate
-                             $donation_allocation->save();
-
-                             $transaction_payout_amount              = $transaction_payout_amount - $amount_to_donate;
-
-                             $objPayout                              = TransactionPayout::find($payout->id);
-                             $objPayout->payout_amount               = $transaction_payout_amount;
-                             $objPayout->save();
-
-
-                             //$objDonation                              = Donation::find($donation->id);
-                             //$objDonation->is_valid                    = 0; 
-                             //$objDonation->save();
-
-
-
 
                         }
-
-                        if ($transaction_payout_amount <= 0) {
-
-
-                             $transaction = Transaction::find($payout->transaction_id);
-                             $transaction->transaction_type_id = 3; 
-                             $transaction->save();
-                             break;
-                             
-
-
-
-
-                        } else {
-
-
-                            $transaction = Transaction::find($payout->transaction_id);
-                            $transaction->transaction_type_id = 2; 
-                            $transaction->save();
-
-
-
-
-                        }
-
-
-
-                        
-                    
-                    }
-
-
-                }
-                else {
-
-
-                    $transaction = Transaction::find($payout->transaction_id);
-                    $transaction->transaction_type_id = 2; 
-                    $transaction->save();
-                    
-
-                }
-
-                
-
-            }
             
 
            
@@ -189,5 +186,7 @@ class Kernel extends ConsoleKernel
 
 
         });
+
+       
     }
 }
